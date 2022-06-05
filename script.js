86e83230f44bd44240562301a877ae4539e1b7f4
Ingrid <98630149+ingridvn@users.noreply.github.com>
2022-06-05 19:26:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-const calculaNota = () =>
+const mensagemAgendamento = () =>
 {
-  const primeiraNota = document.getElementById("n1").value;
-  const segundaNota = document.getElementById("n2").value;
-  const terceiraNota = document.getElementById("n3").value;
-  const quartaNota = document.getElementById("n4").value;
-  /* const quintaNota = document.getElementById("n5").value;
-  const sextaNota = document.getElementById("n6").value; */
-  
-  /* const notaFinal = ((primeiraNota + segundaNota + terceiraNota + quartaNota) / 4).toFixed(1);
-   */
-  let nome = primeiraNota
-  let casv = segundaNota
-  let consulado = terceiraNota
-  let local = quartaNota
- /*  let horaCASV = quintaNota */
- /*  let horaCONS = sextaNota */
+  const pax = document.getElementById("cliente").value;
+  const biometria = document.getElementById("casv").value;
+  const entrevsita = document.getElementById("consulado").value;
+  const placeConsulado = document.getElementById("place").value;
 
-document.getElementById("resultado").innerHTML = `Bom dia ${primeiraNota}, tudo bem? Aqui é a Nicolly da EC Vistos e o seu CASV está agendado para o dia ${segundaNota} e a entrevista no consulado para o dia ${terceiraNota}, em ${quartaNota}. Caso não tenha disponibilidade para esses agendamentos e precise reagendar, me avisa o melhor dia e horário que tentaremos remarcar. Obrigada`
+  let nome = pax
+  let casv = biometria
+  let consulado = entrevsita
+  let local = placeConsulado
+
+
+ if (local === "Rio de Janeiro")
+ {
+  document.getElementById("resultado").innerHTML = `Bom dia ${pax}, tudo bem? Aqui é a Nicolly da EC Vistos e o seu CASV está agendado para o dia ${biometria} e a entrevista no consulado para o dia ${entrevsita}, no ${placeConsulado}. Caso não tenha disponibilidade para esses agendamentos e precise reagendar, me avisa o melhor dia e horário que tentaremos remarcar. Obrigada`
+
+ }
+
+else{
+  document.getElementById("resultado").innerHTML = `Bom dia ${pax}, tudo bem? Aqui é a Nicolly da EC Vistos e o seu CASV está agendado para o dia ${casv} e a entrevista no consulado para o dia ${entrevsita}, em ${placeConsulado}. Caso não tenha disponibilidade para esses agendamentos e precise reagendar, me avisa o melhor dia e horário que tentaremos remarcar. Obrigada`}
+
 }
