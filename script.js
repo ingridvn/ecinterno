279d1226d0493a2036f1ffc2a4e722acf1cd4fbc
Ingrid <98630149+ingridvn@users.noreply.github.com>
2022-06-06 08:51:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ const mensagemAgendamento = () =>
 
  if (local === "Rio de Janeiro")
  {
-  document.getElementById("resultado").innerHTML = `Bom dia ${pax}, tudo bem? Aqui é a Nicolly da EC Vistos e o seu CASV está agendado para o dia ${biometria} e a entrevista no consulado para o dia ${entrevsita}, no ${placeConsulado}. Caso não tenha disponibilidade para esses agendamentos e precise reagendar, me avisa o melhor dia e horário que tentaremos remarcar. Obrigada`
+  document.getElementById("resultado").innerHTML = `${pax}, aqui é a Nicolly da EC Vistos e o seu CASV está agendado para o dia ${biometria} e a entrevista no consulado para o dia ${entrevsita}, no ${placeConsulado}. Caso não tenha disponibilidade para esses agendamentos e precise reagendar, me avisa o melhor dia e horário que tentaremos remarcar. Obrigada`
 
  }
 
 else{
-  document.getElementById("resultado").innerHTML = `Bom dia ${pax}, tudo bem? Aqui é a Nicolly da EC Vistos e o seu CASV está agendado para o dia ${casv} e a entrevista no consulado para o dia ${entrevsita}, em ${placeConsulado}. Caso não tenha disponibilidade para esses agendamentos e precise reagendar, me avisa o melhor dia e horário que tentaremos remarcar. Obrigada`}
+  document.getElementById("resultado").innerHTML = `${pax}, aqui é a Nicolly da EC Vistos e o seu CASV está agendado para o dia ${casv} e a entrevista no consulado para o dia ${entrevsita}, em ${placeConsulado}. Caso não tenha disponibilidade para esses agendamentos e precise reagendar, me avisa o melhor dia e horário que tentaremos remarcar. Obrigada`}
 
 }
